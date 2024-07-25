beea6233ef4b713945ae32b051e2422aa2327b3d
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-07-25 19:17:28 +0200

    chore: remove terraform deployment code

--- deployment/terraform/envs/dev/main.tf
+++
@@ -1,7 +0,0 @@
-module "ip-plz" {
-  source = "../../lambda-deployment-module"
-}
-
-output "prometheus_token" {
-  value = module.ip-plz.api_gateway_invoke_url
-}
--- deployment/terraform/lambda-deployment-module/output.tf
+++
@@ -1,3 +0,0 @@
-output "api_gateway_invoke_url" {
-  value = aws_api_gateway_deployment.ip_plz_v1.invoke_url
-}
